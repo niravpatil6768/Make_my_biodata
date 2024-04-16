44595368a37814d5ef3c3b9245be612208756e43
nirav <patilnirav70@gmail.com>
2024-04-16 10:12:36 +0530

    commit2

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,76 +1,55 @@
 import axios from "axios";
 
-const BASE_URL = "https://139.59.7.235/MarriageBiodata/api/";
-
+const BASE_URL = "http://139.59.7.235/MarriageBiodata/api/";
 
 export const registerService = async (formData) => {
-    try {
-      
-      const response = await axios.post(
-        `${BASE_URL}register`,
-        formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-      );
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  export const loginService = async (formData) => {
-    try {
-        const response = await axios.post(
-            `${BASE_URL}login`,
-            formData,
-            {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                }
-            }
-        );
-        return response.data;
-    }
-    catch(error) {
-        throw error;
-    }
+  try {
+    const response = await axios.post(`${BASE_URL}register`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
   }
+};
 
-  export const checkMobileUserService = async (phone) => {
-    try {
-        const response = await axios.post(
-            `${BASE_URL}checkMobileUser`, 
-        phone,
-        {
-            headers: {
-                "Content-Type" : "multipart/form-data",
-            }
-        }
-        );
-        return response.data;
-    }
-    catch(error) {
-        throw error;
-    }
+export const loginService = async (formData) => {
+  try {
+    const response = await axios.post(`${BASE_URL}login`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
   }
+};
 
-  export const resetPasswordService = async (data) => {
-    try {
-        const response = await axios.post(
-            `${BASE_URL}setPassword`,
-            data,
-            {
-                headers: {
-                    "Content-Type" : "multipart/form-data",
-                }
-            }
-        );
-        return response.data;
-    }
-    catch(error){
-        throw error;
-    }
+export const checkMobileUserService = async (phone) => {
+  try {
+    const response = await axios.post(`${BASE_URL}checkMobileUser`, phone, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
   }
+};
+
+export const resetPasswordService = async (data) => {
+  try {
+    const response = await axios.post(`${BASE_URL}setPassword`, data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
